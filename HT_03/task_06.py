0006a575e2212a866e21ec1e2366b3edbb8feeff
Iryna Sokoliuk <ira-sokoliuk@MacBook-Pro--Iryna.local>
2023-10-24 18:29:07 +0300

    HT_03

--- HT_03/task_06.py
+++ HT_03/task_06.py
@@ -1,11 +1,17 @@
-'''
+"""
 Write a script to get the maximum and minimum value in a dictionary.
-'''
+"""
 
-dict_1 = {'a': 5, 'b': 6, 'c':7, 'd': 10}
+dict_1 = {'a': 5, 'b': 6, 'c': 7, 'd': 10, 'e': [1, 2, 3], 'f': "value", 'g': 123.124}
 
-max_value = max(dict_1.values())
-min_value = min(dict_1.values())
+dict_value = []
+
+for value in dict_1.values():
+    if type(value) in (int, float):
+        dict_value.append(value)
+
+max_value = max(dict_value)
+min_value = min(dict_value)
 
 print(f"Maximum value: {max_value}")
 print(f"Minimum value: {min_value}")
